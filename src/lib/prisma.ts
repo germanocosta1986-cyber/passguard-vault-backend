4ea66c9858a05d3cf7e5875151e637cc0d6bfa2a
germanocosta1986-cyber <germanocosta1986@gmail.comr>
2026-03-17 21:56:57 -0300

    chore: remove generated prisma and fix imports

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,6 @@
-/* import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter }); */
-
+// Mude para isso:
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
 export const prisma = globalForPrisma.prisma || new PrismaClient();
-
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
