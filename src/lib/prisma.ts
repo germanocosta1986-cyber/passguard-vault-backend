a639a89c8dc1569ce1da3d282e147153ad4150c0
germanocosta1986-cyber <germanocosta1986@gmail.comr>
2026-03-17 21:33:17 -0300

    atualização backend prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,16 @@
-import "dotenv/config";
+/* import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter }); */
 
-/* import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; */
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
