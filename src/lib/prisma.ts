edb089ef4e574f93b73f37695ccb132fdcce12a8
germanocosta1986-cyber <germanocosta1986@gmail.comr>
2026-03-17 23:29:32 -0300

    fix: erro src4

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,17 @@
-// Mude para isso:
 import { PrismaClient } from "@prisma/client";
 
+// Forçamos o Prisma a entender que a URL vem do processo de ambiente
+const prismaClientOptions = {
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+};
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient(prismaClientOptions as any);
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
