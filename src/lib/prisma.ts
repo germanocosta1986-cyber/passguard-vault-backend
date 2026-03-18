bf8ca11f8fc2181b45b7900d83435ea7195d9309
germanocosta1986-cyber <germanocosta1986@gmail.comr>
2026-03-18 04:46:58 -0300

    fix: ativando adapter-pg

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+// O 'as any' aqui resolve a incompatibilidade de tipos entre o pg e o adaptador
+const pool = new Pool({ connectionString }) as any;
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  } as any); // O 'as any' evita que o TS trave o build por frescura de tipagem
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
