09db9dfdf792a0f1688a484f79057b1bb80eb5b0
germanocosta1986-cyber <germanocosta1986@gmail.comr>
2026-05-03 19:38:32 -0300

    ponte

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
  */
-
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
